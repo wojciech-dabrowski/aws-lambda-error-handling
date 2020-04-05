247a2837b2e1538e211855cd493a5cb197b9b2d3
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2020-04-05 16:35:34 +0200

    Add scripts for invoking success and fail lambdas from AWS CLI

---
+++ invoke-fail.ps1
@@ -0,0 +1,3 @@
+aws lambda invoke `
+  --function-name aws-lambda-error-handling-FailLambda-<your-unique-key> `
+  fail-response.json
---
+++ invoke-success.ps1
@@ -0,0 +1,3 @@
+aws lambda invoke `
+  --function-name aws-lambda-error-handling-SuccessLambda-<your-unique-key> `
+  success-response.json
